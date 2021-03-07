e93405c67f0b1f4917e54c4d03870008619e0b0d
Louay AD <louayad@outlook.com>
2021-03-08 00:31:24 +0800

    Minor Tweak to 'search.php'

--- search.php
+++ search.php
@@ -4,11 +4,9 @@
 <div class="container" pt-5 pb-5>
 
     <!-- getting the title dynamically from the dashboard -->
-    <h1>Search Results:</h1>
+    <h1>Search Results for '<?php echo get_search_query();?>'</h1>
 
     <!-- we show the search results here | under-development -->
-    <h2><?php printf( __( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>'); ?></h2>
-    
 
 </div>
 
